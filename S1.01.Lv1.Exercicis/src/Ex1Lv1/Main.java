7a1b4b360cb72f25d4de5ee540aa01b6fdd8abe2
ArnauAsole <solenueza@gmail.com>
2025-04-24 10:27:13 +0200

    Update Main.java

--- S1.01.Lv1.Exercicis/src/Ex1Lv1/Main.java
+++ S1.01.Lv1.Exercicis/src/Ex1Lv1/Main.java
@@ -1,11 +1,8 @@
 package Ex1Lv1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         Instrument instrument1 = new Wind("Flute",150);
         Instrument instrument2 = new Percussion("Drums", 220);
         Instrument instrument3 = new StringInstrument("Guitar", 300);
